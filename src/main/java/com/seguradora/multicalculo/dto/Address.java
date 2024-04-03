54f5fff5e6ce68b3a4a7bf10e44584a0cb7c6ba6
rogerioarenas@hotmail.com <@Rodrigo10>
2024-04-03 10:24:01 -0300

    Test: Format maven code clean with example Ktlint

--- src/main/java/com/seguradora/multicalculo/dto/Address.java
+++ src/main/java/com/seguradora/multicalculo/dto/Address.java
@@ -3,7 +3,12 @@ package com.seguradora.multicalculo.dto;
 public class Address {
 
     private String street;
+
     private String city;
+
+
     private String state;
     private String postalCode;
+
+
 }
